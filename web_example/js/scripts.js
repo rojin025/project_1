686388e729957e2e59b5d5c6d4b34abb6f7d2aed
Rojin <ro_jin25@icloud.com>
2018-02-27 11:35:05 -0800

    V1.1
    Added the Name :

--- web_example/js/scripts.js
+++ web_example/js/scripts.js
@@ -1,11 +1,12 @@
 console.log("hello!!!")
 var names = ["Irene", "Rojin", "Jade", "Ed","Luis", "David","Patrick"];
 var email = ["irene.florez@gmail.com","ro_jin25@icloud.com","jade.a.manrique@gmail.com", "edtihcode@gmail.com", "lfr4707@gmail.com", "davidrubioll@gmail.com", "porcuna.patrickj@gmail.com",];
+
 // Hello.lenght will give the length of hello=5
 for (var i = 0; i < names.length; i++) {
   //document.getElementById("text").innerHTML = "test";
-  var htmlName = "<p>" + names[i] + "</p>";
-  var htmlEmail = "<p>" + email[i] + "</p>";
+  var htmlName = "<p>" + " Name: " + names[i] + "</p>";
+  var htmlEmail = "<p>" + "E-mail: " + email[i] + "</p>";
 
   document.getElementById("text").insertAdjacentHTML('beforeend', htmlName);
   document.getElementById("text").insertAdjacentHTML('beforeend', htmlEmail);
