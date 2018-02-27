88a38e392ceca6d8a3f340d232701f1c65630d35
Rojin <ro_jin25@icloud.com>
2018-02-27 10:20:29 -0800

    data

--- web_example/js/scripts.js
+++ web_example/js/scripts.js
@@ -1,1 +1,2 @@
-console.log("hello!")
+var name["Irene", "Rojin", "Jade", "Ed","Luis", "David","Patrick"];
+var email["irene.florez@gmail.com","ro_jin25@icloud.com","jade.a.manrique@gmail.com", "edtihcode@gmail.com", "lfr4707@gmail.com", "davidrubioll@gmail.com", "porcuna.patrickj@gmail.com",];
