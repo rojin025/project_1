433f4b67db787744e31f851775ea08e34a35e477
Rojin <ro_jin25@icloud.com>
2018-02-27 10:59:57 -0800

    data to Html

--- web_example/js/scripts.js
+++ web_example/js/scripts.js
@@ -1,10 +1,14 @@
 console.log("hello!!!")
-var name = ["Irene", "Rojin", "Jade", "Ed","Luis", "David","Patrick"];
+var names = ["Irene", "Rojin", "Jade", "Ed","Luis", "David","Patrick"];
 var email = ["irene.florez@gmail.com","ro_jin25@icloud.com","jade.a.manrique@gmail.com", "edtihcode@gmail.com", "lfr4707@gmail.com", "davidrubioll@gmail.com", "porcuna.patrickj@gmail.com",];
 // Hello.lenght will give the length of hello=5
-for (var i = 0; i < name.length; i++) {
+for (var i = 0; i < names.length; i++) {
   //document.getElementById("text").innerHTML = "test";
-  document.getElementById("text").insertAdjacentHTML('beforeend', name[i] + " ");
-  console.log(name[i]);
-  console.log(email[i]);
+  var htmlName = "<p>" + names[i] + "</p>";
+  var htmlEmail = "<p>" + email[i] + "</p>";
+
+  document.getElementById("text").insertAdjacentHTML('beforeend', htmlName);
+  document.getElementById("text").insertAdjacentHTML('beforeend', htmlEmail);
+  // console.log(name[i]);
+  // console.log(email[i]);
 }
