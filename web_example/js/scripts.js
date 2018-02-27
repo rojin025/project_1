5a4d992973c86ecff70498f77ecef2574459399e
Rojin <ro_jin25@icloud.com>
2018-02-27 10:37:00 -0800

    index
    changing the location of html

--- web_example/js/scripts.js
+++ web_example/js/scripts.js
@@ -1,2 +1,9 @@
-var name["Irene", "Rojin", "Jade", "Ed","Luis", "David","Patrick"];
-var email["irene.florez@gmail.com","ro_jin25@icloud.com","jade.a.manrique@gmail.com", "edtihcode@gmail.com", "lfr4707@gmail.com", "davidrubioll@gmail.com", "porcuna.patrickj@gmail.com",];
+console.log("hello!!!")
+var name = ["Irene", "Rojin", "Jade", "Ed","Luis", "David","Patrick"];
+var email = ["irene.florez@gmail.com","ro_jin25@icloud.com","jade.a.manrique@gmail.com", "edtihcode@gmail.com", "lfr4707@gmail.com", "davidrubioll@gmail.com", "porcuna.patrickj@gmail.com",];
+// Hello.lenght will give the length of hello=5
+for (var i = 0; i < names.length; i++) {
+  document.getElementById("text").innerHTML = "test";
+  console.log(name[i]);
+  console.log(email[i]);
+}
