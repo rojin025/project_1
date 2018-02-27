0a959888554e1334b846ec9e7b578e51bdc779ad
Rojin <ro_jin25@icloud.com>
2018-02-27 10:48:47 -0800

    Added Names to

--- web_example/js/scripts.js
+++ web_example/js/scripts.js
@@ -2,8 +2,9 @@ console.log("hello!!!")
 var name = ["Irene", "Rojin", "Jade", "Ed","Luis", "David","Patrick"];
 var email = ["irene.florez@gmail.com","ro_jin25@icloud.com","jade.a.manrique@gmail.com", "edtihcode@gmail.com", "lfr4707@gmail.com", "davidrubioll@gmail.com", "porcuna.patrickj@gmail.com",];
 // Hello.lenght will give the length of hello=5
-for (var i = 0; i < names.length; i++) {
-  document.getElementById("text").innerHTML = "test";
+for (var i = 0; i < name.length; i++) {
+  //document.getElementById("text").innerHTML = "test";
+  document.getElementById("text").insertAdjacentHTML('beforeend', name[i] + " ");
   console.log(name[i]);
   console.log(email[i]);
 }
