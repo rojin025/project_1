65e3274fac9bfaec818624caea5504ab86b2e9eb
Rojin <ro_jin25@icloud.com>
2018-02-27 11:59:28 -0800

    v.1.4
    ADDing New data to array

--- web_example/js/scripts.js
+++ web_example/js/scripts.js
@@ -1,6 +1,6 @@
 console.log("hello!!!")
-var names = ["Irene", "Rojin", "Jade", "Ed","Luis", "David","Patrick"];
-var email = ["irene.florez@gmail.com","ro_jin25@icloud.com","jade.a.manrique@gmail.com", "edtihcode@gmail.com", "lfr4707@gmail.com", "davidrubioll@gmail.com", "porcuna.patrickj@gmail.com",];
+var names = ["Irene", "Rojin", "Jade", "Ed","Luis", "David","Patrick","Ram"];
+var email = ["irene.florez@gmail.com","ro_jin25@icloud.com","jade.a.manrique@gmail.com", "edtihcode@gmail.com", "lfr4707@gmail.com", "davidrubioll@gmail.com", "porcuna.patrickj@gmail.com","ram@gmail.com"];
 
 // Hello.lenght will give the length of hello=5
 for (var i = 0; i < names.length; i++) {
@@ -13,8 +13,6 @@ for (var i = 0; i < names.length; i++) {
   // var htmlEmail = " <p>" + "E-mail: " + email[i] + "</p></div>";
 
   document.getElementById("text").insertAdjacentHTML('beforeend', html);
-
+  //FOR extra Html and Seperate Email
   // document.getElementById("text").insertAdjacentHTML('beforeend', htmlEmail);
-  // console.log(name[i]);
-  // console.log(email[i]);
 }
