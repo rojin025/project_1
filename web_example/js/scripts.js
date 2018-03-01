e3217dc288f5902827b1f2ecee992215cd5317d9
Rojin BIjukchhe <35796647+rojin025@users.noreply.github.com>
2018-02-28 19:36:48 -0800

    Data
    changes

--- web_example/js/scripts.js
+++ web_example/js/scripts.js
@@ -1,6 +1,6 @@
 console.log("hello!!!")
 var names = ["Irene", "Rojin", "Jade", "Ed","Luis", "David","Patrick","Ram"];
-var email = ["irene.florez@gmail.com","ro_jin25@icloud.com","jade.a.manrique@gmail.com", "edtihcode@gmail.com", "lfr4707@gmail.com", "davidrubioll@gmail.com", "porcuna.patrickj@gmail.com","ram@gmail.com"];
+var email = ["irene@gmail.com","ro_@icloud.com","jaanrique@gmail.com", "edtihc@gmail.com", "lfr4@gmail.com", "dav@gmail.com", "porcuna@gmail.com","ram@gmail.com"];
 
 // Hello.lenght will give the length of hello=5
 for (var i = 0; i < names.length; i++) {
